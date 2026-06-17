d448e12eb009a50e6cfdae805202c902d4842521
cemwatcher2025 <cemwatcher2025@users.noreply.github.com>
2026-06-16 21:49:58 -0500

    Configure GitHub Pages custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === 'true' ? '/enforge-designs/' : '/',
+  base: process.env.CUSTOM_DOMAIN === 'true' ? '/' : process.env.GITHUB_PAGES === 'true' ? '/enforge-designs/' : '/',
   plugins: [react()],
 })
